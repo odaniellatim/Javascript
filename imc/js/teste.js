263acdeecf9b5b245ba125a71cff9d261457dd44
Daniel Latim <141602397+odaniellatim@users.noreply.github.com>
2025-06-02 18:04:54 -0300

    Update Junho
    Update Junho/25

--- imc/js/teste.js
+++ imc/js/teste.js
@@ -1,50 +1,49 @@
 // 📌 Funções matemáticas básicas
 function soma(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtracao(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiplicacao(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divisao(a, b) {
-    if (b === 0 || a === 0) {
-        return 'erro divisão por 0'; 
-    }
-    return a / b;
-    
-    
+  if (b === 0 || a === 0) {
+    return "erro divisão por 0";
+  }
+  return a / b;
 }
 
 // 📌 Função para cálculos mais complexos
 function calculoEspecial(a, b, c) {
-    return (a * b) + c;
+  return a * b + c;
 }
 
 // 📌 Função principal que organiza os cálculos
 function executarCalculos(num1, num2) {
-        let a = num1;
-        let b = num2;
-        let c = 5
+  let a = num1;
+  let b = num2;
+  let c = 5;
 
-        const somaValores = soma(num1, num2);
-        const subtracaoValores = subtracao(num1, num2);
-        const multiplicacaoValores = multiplicacao(num1, num2);
-        const divisaoValores = divisao(num1, num2);
-        const calculoEspecialValores = calculoEspecial(num1, num2, c);
-        
-        const resultado1 = `A soma de ${a} + ${b} é: `+ somaValores;
-        const resultado2 = `A subtracao de ${a} - ${b} é: `+ subtracaoValores;
-        const resultado3 = `A multiplicacao de ${a} * ${b} é: `+ multiplicacaoValores;
-        const resultado4 = `A divisao de ${a} / ${b} é: `+ divisaoValores;
-        const resultado5 = `A calculoEspecial de ${a} * ${b} + ${c} é: `+ calculoEspecialValores;
+  const somaValores = soma(num1, num2);
+  const subtracaoValores = subtracao(num1, num2);
+  const multiplicacaoValores = multiplicacao(num1, num2);
+  const divisaoValores = divisao(num1, num2);
+  const calculoEspecialValores = calculoEspecial(num1, num2, c);
 
-    
-    return resultado1 +resultado2 + resultado3 + resultado4 + resultado5;
+  const resultado1 = `A soma de ${a} + ${b} é: ` + somaValores;
+  const resultado2 = `A subtracao de ${a} - ${b} é: ` + subtracaoValores;
+  const resultado3 =
+    `A multiplicacao de ${a} * ${b} é: ` + multiplicacaoValores;
+  const resultado4 = `A divisao de ${a} / ${b} é: ` + divisaoValores;
+  const resultado5 =
+    `A calculoEspecial de ${a} * ${b} + ${c} é: ` + calculoEspecialValores;
+
+  return resultado1 + resultado2 + resultado3 + resultado4 + resultado5;
 }
 
 // 📌 Testando com os valores dados
---
+++ testes-codigo/index.js
@@ -0,0 +1,14 @@
+/*
+
+Arquivo para testes de pequenos codigos.
+
+*/
+console.log(""); // separador para deixar a leitura no terminal mais legivel.
+console.log("Arquivo para testes de pequenos codigos javascript");
+console.log(""); // separador para deixar a leitura no terminal mais legivel.
+
+let nome = "Daniel Latim";
+
+if (nome == "Daniel Latim") {
+  console.log(nome);
+}
