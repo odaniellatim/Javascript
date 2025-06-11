63e547fe36fc1a511f9badc5c9c288e61f2c1004
Daniel Latim <141602397+odaniellatim@users.noreply.github.com>
2025-06-11 15:50:53 -0300

    Update 11/06/25
    Update 11/06/25

--- testes-codigo/main.js
+++ testes-codigo/main.js
@@ -1,26 +1,20 @@
 const tagHtml = document.querySelector(".cadastro");
 const button = tagHtml.querySelector(".button");
 const inputTexto = tagHtml.querySelector(".texto-item");
+let id = 1; // contador
+let textArray = []; // array de itens na lista
 
 // seleciona a classe lista html
 const listaItems = document.querySelector(".lista");
 listaItems.innerText = "";
 listaItems.appendChild(criaDiv());
 
-function cadastroTexto() {
-  // seleciona a classe alinhamento html nova
-  const divAlign = document.querySelector(".alinhamento");
-
-  const p = divAlign.appendChild(criaP());
-  p.innerText = inputTexto.value;
-
-  indiceItens();
-
-  // Limpa o input de Texto
-  limpaInput();
-}
-
 button.addEventListener("click", cadastroTexto);
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    cadastroTexto();
+  }
+});
 
 function criaDiv() {
   const criaDivElement = document.createElement("div");
@@ -31,24 +25,37 @@ function criaDiv() {
 function criaP() {
   const criaElement = document.createElement("p");
   criaElement.classList.add("item");
-  // criaElement.innerText = indiceItens();
   return criaElement;
-}
-
-function indiceItens() {
-  let lista = [];
-  // lista = document.querySelectorAll("p");
-
-  lista.push(inputTexto.value);
-
-  console.log(lista);
-  for (let indice in lista.length) {
-    console.log(typeof indice, indice);
-    return indice;
-  }
 }
 
 function limpaInput() {
   inputTexto.value = "";
   inputTexto.focus();
+}
+
+function cadastroTexto() {
+  if (inputTexto.value === "") return;
+  // seleciona a classe alinhamento html nova
+  const divAlign = document.querySelector(".alinhamento");
+  const p = divAlign.appendChild(criaP());
+  const TextoInputValor = inputTexto.value.toUpperCase();
+
+  cadastroArray(TextoInputValor);
+  formataTextoLista(id, p, TextoInputValor);
+
+  // Limpa o input de Texto
+  limpaInput();
+
+  // soma um valor na variavel id para criar os indice na lista de itens
+  id = id + 1;
+}
+
+function formataTextoLista(id, p, TextoInputValor) {
+  const resultTextoLista = (p.innerHTML =
+    `<b>${id}</b>. ${TextoInputValor}`.toUpperCase());
+  return resultTextoLista;
+}
+
+function cadastroArray(textInput) {
+  textArray.push(textInput);
 }
