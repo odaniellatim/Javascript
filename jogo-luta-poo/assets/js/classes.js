77d2d8a431ad54d07a8748e588d5438fd6c1133c
Daniel Latim <141602397+odaniellatim@users.noreply.github.com>
2025-07-14 01:57:05 -0300

    update 14/07/25
    update 14/07/25

--- jogo-luta-poo/assets/js/classes.js
+++ jogo-luta-poo/assets/js/classes.js
@@ -70,31 +70,21 @@ class Stage {
   start() {
     this.update();
 
-    this.fighter1El
-      .querySelector(".attackButton")
-      .addEventListener("click", () =>
-        this.doAttack(this.fighter1, this.fighter2)
-      );
+    this.fighter1El.querySelector(".attackButton").addEventListener("click", () => this.doAttack(this.fighter1, this.fighter2));
 
-    this.fighter2El
-      .querySelector(".attackButton")
-      .addEventListener("click", () =>
-        this.doAttack(this.fighter2, this.fighter1)
-      );
+    this.fighter2El.querySelector(".attackButton").addEventListener("click", () => this.doAttack(this.fighter2, this.fighter1));
   }
 
   update() {
     //Fighter 1
-    this.fighter1El.querySelector(".name").innerHTML = `${
-      this.fighter1.name
-    } - ${this.fighter1.life.toFixed(1)} HP`;
+    this.fighter1El.querySelector(".name").innerHTML = `${this.fighter1.name} - 
+    ${this.fighter1.life.toFixed(1)} HP`;
+
     let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
     this.fighter1El.querySelector(".bar").style.width = `${f1Pct}%`;
 
     //Fighter 2
-    this.fighter2El.querySelector(".name").innerHTML = `${
-      this.fighter2.name
-    } - ${this.fighter2.life.toFixed(1)} HP`;
+    this.fighter2El.querySelector(".name").innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
     let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
     this.fighter2El.querySelector(".bar").style.width = `${f2Pct}%`;
   }
@@ -114,11 +104,7 @@ class Stage {
 
     if (actualAttack > actualDefense) {
       attacked.life -= actualAttack;
-      this.log.addMenssage(
-        `${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${
-          attacked.name
-        }`
-      );
+      this.log.addMenssage(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`);
     } else {
       this.log.addMenssage(`${attacked.name} conseguiu defender...`);
     }
--- testes-codigo/main.js
+++ testes-codigo/main.js
@@ -1,116 +1,44 @@
-const tagHtml = document.querySelector(".cadastro");
-
-const inputTexto = tagHtml.querySelector(".texto-item");
-const inputValor = tagHtml.querySelector(".valor-item");
-
-const button = tagHtml.querySelector(".button");
-
-const totalItensLista = document.querySelector(".total-produto");
-const divValorTotal = document.querySelector(".valor-total");
-
-let id = 1; // contador
-let textArray = []; // array de itens na lista
-let valorArray = []; // array para soma total do valor itens;
-
-// seleciona a classe lista html
-const listaItems = document.querySelector(".lista");
-listaItems.innerHTML = ""; // reseta div lista
-//cria a div no container
-listaItems.appendChild(criaDiv());
-
-button.addEventListener("click", cadastroTexto);
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    cadastroTexto();
-  }
-});
-
-function carrinhoTotal() {
-  if (id === "1") {
-    totalItensLista.innerHTML = `<b>${id}</b> - Produto no carrinho`;
-  } else {
-    totalItensLista.innerHTML = `<b>${id}</b> - Produtos no carrinho`;
-  }
-}
-
-function cadastroTexto() {
-  //Validação Campos em Branco
-  if (!inputTexto.value && !inputValor.value) {
-    inputTexto.style.borderColor = "";
-    inputTexto.style.borderColor = "red";
-    inputValor.style.borderColor = "red";
-    return;
-  } else if (!inputTexto.value) {
-    inputTexto.style.borderColor = "";
-    inputTexto.style.borderColor = "red";
-    return;
-  } else if (!inputValor.value) {
-    inputTexto.style.borderColor = "";
-    inputValor.style.borderColor = "red";
-    return;
+class NomeUser {
+  constructor(nome, sobreNome) {
+    this.nome = nome;
+    this.sobreNome = sobreNome;
   }
 
-  // seleciona a classe alinhamento html nova
-  const divAlign = document.querySelector(".alinhamento");
-  const p = divAlign.appendChild(criaP());
-  const textoInputValor = inputTexto.value.toUpperCase();
-  const valInput = Number(inputValor.value).toFixed(2);
+  nomeCompleto() {
+    let inputNome = this.nome.value;
+    let inputSobrenome = this.sobreNome.value;
+    const div = document.querySelector(".nomeCompleto");
 
-  cadastroArray(textoInputValor + "," + valInput);
+    // if (inputNome && inputSobrenome) {
+    // }
 
-  // Apresenta texto input tela
-  const ar = formataTextoLista(p, textoInputValor, valInput);
-
-  //Carrinho de compras total itens
-  carrinhoTotal();
-
-  //Valor total Carrinho
-  const somaItens = totalValorTotalCarrinho().toFixed(2);
-  divValorTotal.innerHTML = `Total Carrinho: <b>${somaItens}</b>`;
-
-  // Limpa o input de Texto
-  limpaInput();
-
-  // soma um valor na variavel id para criar os indice na lista de itens
-  id = id + 1;
-}
-
-function totalValorTotalCarrinho() {
-  saldo = Number(inputValor.value);
-  valorArray.push(saldo);
-  sum = 0;
-  for (let i = 0; i < valorArray.length; i++) {
-    sum += valorArray[i];
+    if (!inputNome) {
+      return (div.innerHTML = "<strong>Nome está vazio</strong>");
+    }
+    if (!inputSobrenome) {
+      return (div.innerHTML = "<strong>Sobrenome está vazio</strong>");
+    } else {
+      let nc = `<span>${inputNome} ${inputSobrenome}</span>`;
+      return nc;
+    }
   }
-  return sum;
+
+  imprimeNome() {
+    const div = document.querySelector(".nomeCompleto");
+    const nomeImpresso = (div.innerHTML = `<b>${this.nomeCompleto()}</b>`);
+
+    return nomeImpresso;
+  }
+
+  start() {
+    const button = document.querySelector(".button");
+
+    button.addEventListener("click", () => {
+      this.imprimeNome();
+    });
+  }
 }
 
-function formataTextoLista(p, textoInputValor, valInput) {
-  const resultTextoLista =
-    (p.innerHTML = `${textoInputValor} <span class="price"> R$ ${valInput}<span>`);
-  return resultTextoLista;
-}
+const user1 = new NomeUser(document.querySelector("#name"), document.querySelector("#lastname"));
 
-function cadastroArray(textInput) {
-  return textArray.push(textInput);
-}
-
-function criaDiv() {
-  const criaDivElement = document.createElement("div");
-  criaDivElement.classList.add("alinhamento");
-  return criaDivElement;
-}
-
-function criaP() {
-  const criaElement = document.createElement("p");
-  criaElement.classList.add("item");
-  return criaElement;
-}
-
-function limpaInput() {
-  inputTexto.value = "";
-  inputValor.value = "";
-  inputTexto.style.borderColor = "";
-  inputValor.style.borderColor = "";
-  inputTexto.focus();
-}
+user1.start();
