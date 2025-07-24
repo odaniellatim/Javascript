c3cf68eee77cbe693762c74429ebdf295bd731c7
Daniel Latim <141602397+odaniellatim@users.noreply.github.com>
2025-07-24 13:59:41 -0300

    update 24/07/25

--- aula-curso-consulta/estudos-codigos/assets/js/mais.js
+++ aula-curso-consulta/estudos-codigos/assets/js/mais.js
@@ -1,19 +1,22 @@
 //classe
 class Pessoa {
-  static resultado = document.querySelector("#resultado");
-  static btn = document.querySelector("#button");
-  static inputNome = document.querySelector(".nome");
+  //static resultado = document.querySelector("#resultado");
+  //static btn = document.querySelector("#button");
+  // static inputNome = document.querySelector(".nome");
 
-  constructor(date) {
-    this.date = date;
+  constructor(dataAtual, resultado, btn, inputNome) {
+    this.date = dataAtual;
     this.nome = "";
+    this.btn = btn;
+    this.inputNome = inputNome;
+    this.resultado = resultado;
     // if (this.constructor === Pessoa) {
     //   console.warn("A classe Pessoa não pode ser instanciada.");
     //   throw new ReferenceError("A classe Pessoa não pode ser instanciada.");
     // }
   }
 
-  static setNome(nome) {
+  setNome(nome) {
     return (this.nome = nome);
   }
 
@@ -24,17 +27,30 @@ class Pessoa {
     return this.date;
   }
 
+  //mesagem de erro e validação
+  mensagem(erro, msg) {
+    return alert(`${erro}: ${msg}`);
+  }
+
   //função alterar o nome
-  static AtualizarNome() {
-    const valorInput = Pessoa.inputNome.value;
-    Pessoa.resultado.innerHTML += `${Pessoa.setNome(valorInput)} - ${p1.setDate()}<br/>`;
-    Pessoa.inputNome.value = "";
-    Pessoa.inputNome.focus();
+  AtualizarNome() {
+    //console.log(this.inputNome);
+    const valorInput = document.querySelector(".nome").value;
+    console.log(valorInput);
+
+    if (valorInput !== "") {
+      this.resultado.innerHTML += `${this.setNome(valorInput)} - ${this.setDate()}<br/>`;
+      this.inputNome.value = "";
+      this.inputNome.focus();
+      this.mensagem(1, "Cadastro realizado com sucesso.");
+    } else {
+      return this.mensagem(1, "Preencha o campo corretamente.");
+    }
   }
 
   //Método para chamar os eventos de click
-  static EventosButton() {
-    Pessoa.btn.addEventListener("click", Pessoa.AtualizarNome);
+  EventosButton() {
+    this.btn.addEventListener("click", this.AtualizarNome);
 
     document.addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
@@ -45,9 +61,12 @@ class Pessoa {
 }
 
 const dataAtual = Date.now();
+const resultado = document.querySelector("#resultado");
+const btn = document.querySelector("#button");
+const inputNome = document.querySelector(".nome");
 
 //instancia objeto
-const p1 = new Pessoa(dataAtual);
+const p1 = new Pessoa(dataAtual, resultado, btn, inputNome);
 
 //Chama a função que verifica se o botão foi clicado
-Pessoa.EventosButton();
+p1.EventosButton();
