dc5db332508a0c2f3706ee92f29b8c48fe8afacf
Daniel Latim <141602397+odaniellatim@users.noreply.github.com>
2025-09-24 23:53:39 -0300

    update bingo

--- bingo/app.js
+++ bingo/app.js
@@ -3,29 +3,29 @@
 
 // O array vai ser usado para representar o numero selecionado pelo usuario.
 const numeroCartela = [];
-const numeroMaximo = 50
+const numeroMaximo = 50 + 1
 const numerosSorteado = [];
 
 //Randomizar os numeros da cartela
-function RandomizarNumero(numeroMaximo){
-   
-   for(let i=0; i=numeroMaximo; i++){
+function RandomizarNumero(numeroMaximo) {
+
+    for (let i = 0; i < numeroMaximo; i++) {
 
         // gera um numero aleatório usando o numero maximo da cartela do usuario.
         const numero = Math.floor(Math.random() * numeroMaximo)
 
         // verifica se o numero já está no array e repete o loop se existir
-        if (numeroCartela.includes(numero)){
+        if (numeroCartela.includes(numero)) {
             i--;
             continue;
-        }else{
+        } else {
             // cadastra o numero aleatório no array para construir a cartela aleatória
-            numeroCartela.push(numero)
+            numeroCartela.push(numero);
         }
-   }
+    }
 }
 
 RandomizarNumero(numeroMaximo)
 
 console.log(numeroCartela)
-console.log(numeroCartela.sort((a, b) => a-b))
+console.log(numeroCartela.sort((a, b) => a - b))
